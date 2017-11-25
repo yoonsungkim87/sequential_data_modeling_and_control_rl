43cbd945535798b6023e6af143e43489ab96676c
Yoon-Sung Kim <yoonsung.kim87@gmail.com>
2017-11-25 15:15:01 +0900

    initial_commit

---
+++ lstm.py
@@ -0,0 +1,63 @@
+import os
+import time
+import warnings
+import numpy as np
+
+#scikit-learn
+from sklearn import preprocessing
+
+#keras
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Dropout, Reshape
+from keras.layers.wrappers import TimeDistributed
+from keras.layers.recurrent import LSTM
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #Hide messy TensorFlow warnings
+warnings.filterwarnings("ignore") #Hide messy Numpy warnings
+
+def load_data(path, sequence_length, row_start, column_start, output_border_start, do_normalize):
+    with open(path) as f:
+        content = f.readlines()
+    content = [a[0].split(",")for a in [x.strip().split("\n") for x in content]]
+    np_content = np.array(content)
+    np_content = np_content[row_start-1:].transpose()[column_start-1:].transpose()
+    np_content = np.asarray(np_content, dtype=np.float32)
+    #normalize
+    if do_normalize:
+        np_content, info = normalize(np_content)
+    else:
+        info = None
+    #manipulating
+    x_temp = []
+    y_temp = []
+    for i in range(np_content.shape[0] - sequence_length):
+        x_temp.append(np_content[i:i+sequence_length,:output_border_start-1])
+        y_temp.append(np_content[i+sequence_length:i+sequence_length+1,output_border_start-1:])
+    return np.array(x_temp), np.array(y_temp), info
+
+def normalize(data):
+    m = np.mean(data, axis=0)
+    s = np.std(data, axis=0)
+    i = [m, s]
+    d = preprocessing.scale(data)
+    return d, i
+
+def build_model(batch_size, sequence_length, x_dim, h_dim, num_hid_lay, y_dim):
+    model = Sequential()
+    model.add(LSTM(output_dim=h_dim, return_sequences=True, stateful=True, init='uniform', 
+                   batch_input_shape=(batch_size, sequence_length, x_dim)))
+    for i in range(num_hid_lay):
+        model.add(LSTM(output_dim=h_dim, return_sequences=True, stateful=True, init='uniform'))
+    model.add(Reshape((1, -1)))
+    model.add(TimeDistributed(Dense(y_dim)))
+    model.add(Activation('linear'))
+    start = time.time()
+    model.compile(loss='mean_squared_error', optimizer='rmsprop')
+    #sgd = SGD(lr=0.1, decay=1e-4, momentum=0.2, nesterov=True)
+    #model.compile(loss='mean_squared_error', optimizer=sgd)
+    print("> Compilation Time : ", time.time() - start)
+    return model
+
+def predict_sequence(model, x_test, batch_size):
+    pred = model.predict(x_test, batch_size=batch_size)
+    return pred
---
+++ run.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import lstm
+
+def plot_results(predicted_data, true_data):
+    fig = plt.figure(facecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data')
+    plt.plot(predicted_data, label='Prediction')
+    plt.legend()
+    plt.show()
+
+seq_len = 10
+train_samples = 10000
+test_samples = 100
+    
+x_raw, y_raw, info = lstm.load_data(path="./COP_Data.csv", sequence_length = seq_len, row_start=2, column_start=2, output_border_start=55, do_normalize=True)
+
+#print(x_raw.shape, y_raw.shape)
+
+x_dim = x_raw.shape[2]
+y_dim = y_raw.shape[2]
+x_train, y_train = x_raw[:train_samples,:,:], y_raw[:train_samples,:,:]
+x_test, y_test = x_raw[-test_samples:,:,:], y_raw[-test_samples:,:,:]
+
+m_ = lstm.build_model(1, seq_len, x_dim, 100, 1, y_dim)
+m_.fit(x_train, y_train, batch_size=1, nb_epoch=10)
+y_pred = lstm.predict_sequence(m_, x_test, batch_size=1)
+
+for i in range(y_dim):
+    plot_results(y_pred.reshape(-1, y_dim).transpose()[i], y_test.reshape(-1, y_dim).transpose()[i])
