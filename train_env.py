91b8c5fdd70f68e570106a9f9ea1a5117e57ff77
vm2 <yoonsung.kim87@gmail.com>
2017-12-28 09:07:26 +0900

    few_changes

--- train_env.py
+++ train_env.py
@@ -3,14 +3,6 @@
 import matplotlib.pyplot as plt
 import lstm
 import numpy as np
-import keras
-
-class LossHistory(keras.callbacks.Callback):
-    def init(self):
-        self.losses = []
-        
-    def on_epoch_end(self, batch, logs={}):
-        self.losses.append(logs.get('loss'))
 
 def plot_results(predicted_data, true_data):
     fig = plt.figure(facecolor='white')
@@ -40,12 +32,9 @@ y_dim = y_raw.shape[2]
 x_train, y_train = x_raw[:train_samples,:,:], y_raw[:train_samples,:,:]
 x_test, y_test = x_raw[-test_samples:,:,:], y_raw[-test_samples:,:,:]
 
-history = LossHistory()
-history.init()
-
 m_ = lstm.build_model(1, seq_len, x_dim, 100, 1, y_dim, False)
 #m_.load_weights("./save_model/env.h5")
-m_.fit(x_train, y_train, batch_size=1, nb_epoch=10, verbose=2, callbacks=[history])
+m_.fit(x_train, y_train, batch_size=1, nb_epoch=10)
 m_.save_weights("./save_model/env.h5")
 y_pred = lstm.predict_sequence(m_, x_test, batch_size=1)
 
